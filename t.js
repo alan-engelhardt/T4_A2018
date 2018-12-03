8d8939c199daa213eef5a4ec6f1c08a59ea2c803
Alan Engelhardt <Alan@alansmbamid2014.home>
2018-12-03 18:02:53 +0100

    add animation to SVG snow

--- t.js
+++ t.js
@@ -1,19 +1,35 @@
 const bg = document.querySelector("#bg")
 const svg = document.querySelector("template").content
+let cnt = 0
 
 function addFlake(){
 	const clone = svg.cloneNode(true)
 	const flake = clone.querySelector(".star")
 	const randomX = Math.random()*100
-	const randomY = Math.random()*100
-	flake.style.transform = "scale("+Math.random()*1.5+")"
-	flake.style.top = randomX+"%"
+	const randomD = Math.random()+1
+	const randomS = Math.random()
 
-	flake.style.left = randomY+"%"
 	flake.style.transform = "rotate("+Math.random()*359+"deg)"
-	bg.appendChild(clone)
-}
+	flake.style.left = randomX+"%"
+	flake.style.top = 0
+	let l=0
+	let intv = setInterval(function(){
+		l++
+		flake.style.top = randomD*l/10 + "%"
+		flake.style.transform = "rotate("+l*5+"deg) scale("+randomS+")"
+		bg.appendChild(clone)
+		if(randomD*l >= 1000){
+			clearInterval(intv)
+			console.log(bg.lastChild)
+			bg.removeChild(bg.lastChild)
+		}
+	}, 10)
+	}
 
-for(let i=0; i<500; i++){
-	addFlake()
-}
+let snowIntv = setInterval(function(){
+	cnt++
+	addFlake();
+	if(cnt>500){
+	   clearInterval(snowIntv)
+	   }
+}, 10)
