c136bcb357db1aac1c7d306e67a8c12f93796b87
Alan Engelhardt <Alan@windows-alf143u.kea.local>
2018-12-04 09:59:25 +0100

    random CSS snow all start simultaniously

--- t.js
+++ t.js
@@ -1,23 +1,33 @@
-const bg = document.querySelector("#bg")
+const bg = document.querySelector("#bg");
 const flake0 = document.querySelector(".flake")
 let cntFlakes = 0
-let ypos = 0
 
-function fall(){
-	ypos++
-	flake0.style.top = ypos + "px"
-	const max = bg.clientHeigt
-	if(ypos > max){
-		ypos = 0
+console.log(bg)
+
+function snow(flake){
+	const rand = Math.random()
+	let ypos = 0
+	function fall(){
+		ypos++
+		flake.style.top = ypos/3*rand + "vh"
+		if(ypos/3*rand > 100){
+			ypos = 0
+		}
+
+		requestAnimationFrame(fall)
 	}
-	requestAnimationFrame(fall)
+	fall()
 }
+
 
 function addFlake(){
 	const newFlake = flake0.cloneNode(true)
 	newFlake.style.left = Math.random()*100 + "vw"
 	newFlake.style.transform = "scale("+Math.random()+")"
 	bg.appendChild(newFlake)
+	snow(newFlake)
 }
 
-fall()
+for(let i=0; i<200; i++){
+	addFlake()
+}
