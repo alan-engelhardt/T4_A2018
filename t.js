c06002f9f8a82ded6a93cbe1a3f03892d8a51e16
Alan Engelhardt <Alan@windows-alf143u.kea.local>
2018-12-04 09:36:58 +0100

    one CSS flake falling

--- t.js
+++ t.js
@@ -1,40 +1,23 @@
 const bg = document.querySelector("#bg")
-const temp = document.querySelector("template").content
-let cnt = 0
+const flake0 = document.querySelector(".flake")
+let cntFlakes = 0
+let ypos = 0
 
-function addFlake(){
-	const clone = temp.cloneNode(true)
-	const flake = clone.querySelector(".flake")
-	const randomX = Math.random()*100
-	const randomD = Math.random()+1
-	const randomS = Math.random()
-	//flake.style.transform = "scale("+randomS+")"
-	flake.style.left = randomX+"%"
-	flake.style.top = 0
-	let l=0
-	let intv = setInterval(function(){
-		l++
-		flake.id="f"+l
-		//flake.style.top = randomD*l/10 + "%"
-		flake.style.transform = "translateY("+randomD*l/10+"vh) scale("+randomS+")"
-		bg.appendChild(clone)
-		if(l >= 900){
-			l=0
-		}
-	}, 10)
+function fall(){
+	ypos++
+	flake0.style.top = ypos + "px"
+	const max = bg.clientHeigt
+	if(ypos > max){
+		ypos = 0
 	}
-/*
-for(let i=0; i<100; i++){
-	addFlake()
+	requestAnimationFrame(fall)
 }
 
-*/
+function addFlake(){
+	const newFlake = flake0.cloneNode(true)
+	newFlake.style.left = Math.random()*100 + "vw"
+	newFlake.style.transform = "scale("+Math.random()+")"
+	bg.appendChild(newFlake)
+}
 
-let snowIntv = setInterval(function(){
-	cnt++
-	addFlake();
-	if(cnt>200){
-		clearInterval(snowIntv)
-	}
-}, 50)
-
+fall()
